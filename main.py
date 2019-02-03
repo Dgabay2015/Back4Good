13bad629a68b7b0a4c6d67c5a3dd4805a8a4df9f
ogbautista <ogbautista@gmail.com>
2019-02-02 23:59:45 -0500

    created route ('/', methods=['POST'])

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import random, sqlite3
 app = Flask(__name__)
 
 @app.route('/')
+def my_form():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
 def my_form_post():
 
     conn = sqlite3.connect('data.sqlite')
