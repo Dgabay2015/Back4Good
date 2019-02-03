963ad9c2eec0a0ba52b2d271d76ae9da15377b69
ogbautista <ogbautista@gmail.com>
2019-02-02 23:54:59 -0500

    Added location records

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import random, sqlite3
 
 app = Flask(__name__)
 
-#@app.route('/')
-def my_form_1():
-    return render_template('index.html')
-
 @app.route('/')
 def my_form_post():
 
@@ -14,21 +10,32 @@ def my_form_post():
     cur = conn.cursor()
 
     cur.execute('CREATE TABLE IF NOT EXISTS Challenges (id INTEGER PRIMARY KEY, challenge TEXT)')
+
     record_ids = []
     cur.execute('SELECT id from Challenges')
-
     try:
         for record in cur:
             record_ids.append(record[0])
-
     except:
         print("Unable to read challenges from data.sqlite")
-
     rnd_index = random.choice(record_ids)
 
     cur.execute('SELECT challenge FROM Challenges WHERE id=?', (rnd_index,))
     record = cur.fetchone()
     challenge = record[0]
-    print(challenge)
-    return render_template('index.html', challenge = challenge)
-    #return render_template('index.html')
+
+    record_ids = []
+    cur.execute('SELECT id from Places')
+    try:
+        for record in cur:
+            record_ids.append(record[0])
+    except:
+        print("Unable to read places from data.sqlite")
+    rnd_index = random.choice(record_ids)
+
+    cur.execute('SELECT place FROM Places WHERE id=?', (rnd_index,))
+    record = cur.fetchone()
+    place = record[0]
+
+    print(challenge, place)
+    return render_template('index.html', challenge = challenge, location = place)
